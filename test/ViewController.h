3506bccdb66a506b2ad2e4f32f15fe07561894d8
AtMyCode <zhanglijian2013@163.com>
2015-08-11 10:35:53 +0800

    sdfsa
    afd

---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by ss on 15/8/6.
+//  Copyright (c) 2015年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITabBarController
+
+@end
---
+++ test/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by ss on 15/8/6.
+//  Copyright (c) 2015年 ss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tabBar.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_footer"]];
+    self.tabBar.tintColor = [UIColor clearColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
